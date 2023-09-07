edd36e4f15dc258c8286ef223d0e437d2dd85ec2
Кулинарная Джаба <bugdeer11@gmail.com>
2023-09-07 21:52:35 +0300

    1.0.1 exp

---
+++ php/index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Главная</title>
+    <link rel="stylesheet" href="../html/css/blocks.css">
+<!--    <link rel="stylesheet" href="../html/css/main.css">-->
+</head>
+<body>
+
+<?php
+    require "Header.php";
+?>
+
+<div class="input-er">
+    <h1>Lab 1</h1>
+    <form action="" method="post" >
+        <div class="input-box">
+            <input type="text" name="" required="">
+            <label > Введите R:</label>
+        </div>
+        <div class="input-box">
+            <input type="text" name="" required="">
+            <label> Введите X:</label>
+        </div>
+        <div class="input-box">
+            <input type="text" name="" required="">
+            <label > Введите Y:</label>
+        </div>
+        <br>
+        <div>
+            <button type="submit" class="butSub">
+                Request
+            </button>
+            <a href="#">
+
+            </a>
+        </div>
+    </form>
+</div>
+
+</body>
+</html>
---
+++ php/info.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Инфо</title>
+    <link rel="stylesheet" href="../html/css/blocks.css">
+</head>
+<body>
+
+<?php
+require "Header.php";
+?>
+
+<div class="cube">
+    kube 1
+    <div class="cube">
+        kube 2
+    </div>
+</div>
+<div class="cube">
+    kube 1
+</div>
+</body>
+</html>
